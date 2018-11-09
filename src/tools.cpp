e080890cd26ca2b423336b6b829d53319cdd8732
roberto.maestre <roberto.maestre@bbvadata.com>
2018-11-09 20:59:02 +0100

    Haning filter added

--- src/tools.cpp
+++ src/tools.cpp
@@ -40,11 +40,11 @@ DataFrame apodization(arma::vec frequences, String filter) {
     factor = 1 - arma::abs(frequencesCentered) / middle;
   } else if (filter == "blackman") {
     factor = 21.0 / 50.0 + 0.5 * arma::cos(M_PI * frequencesCentered / middle) +
-      2.0 / 25.0 * arma::cos(2 * M_PI * frequencesCentered / middle);
+      2.0 / 25.0 * arma::cos(2.0 * M_PI * frequencesCentered / middle);
   } else if (filter == "connes") {
-    factor = arma::pow(1 - arma::pow((frequencesCentered / middle), 2), 2);
+    factor = arma::pow(1.0 - arma::pow((frequencesCentered / middle), 2), 2);
   } else if (filter == "cosine") {
-    factor = arma::cos((M_PI * frequencesCentered) / (2 * middle));
+    factor = arma::cos((M_PI * frequencesCentered) / (2.0 * middle));
   } else if (filter == "gaussian") {
     factor = arma::exp(
       -0.5 *
@@ -52,6 +52,9 @@ DataFrame apodization(arma::vec frequences, String filter) {
   } else if (filter == "hamming") {
     factor = 27.0 / 50.0 +
       23.0 / 50.0 * arma::cos(M_PI * frequencesCentered / middle);
+  } else if (filter == "hanning") {
+    factor =
+      arma::pow(arma::cos((M_PI * frequencesCentered) / (2.0 * middle), 2);
   } else {
     factor = 1;
   }
