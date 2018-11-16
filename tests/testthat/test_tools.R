7202b51fb3d304ff1dce419723ef0712a21ed760
roberto.maestre <roberto.maestre@bbvadata.com>
2018-11-16 20:11:36 +0100

    New tests added

--- tests/testthat/test_tools.R
+++ tests/testthat/test_tools.R
@@ -1,5 +1,7 @@
-context("Histogram of differences")
 library(variableStars)
+
+
+context("Differences tools")
 
 test_that("differences returns an empty vector if all differences are 0", {
   for (i in seq(1, 100)) {
@@ -54,19 +56,35 @@ test_that("differences returns the combination of all differences without zeros"
             expect_equal(diffs[5], abs(numberC - numberD))
           })
 
+
+test_that("adjacentDifferences returns the same output as diff",
+          {
+            for (i in seq(1, 100)) {
+              x <-
+                sample(seq(from = 0,
+                           to = 100,
+                           by = 1),
+                       size = round(runif(1, min = 10, max = 40)),
+                       replace = TRUE)
+              # Check same output
+              expect_true(all(adjacentDifferences(x) == diff(x)))
+            }
+          })
+
+
+context("Differences histogram")
+
 test_that("diffHistogram returns a histogram with equal length between bins and values",
           {
-            for (i in seq(1, 50)) {
+            for (i in seq(1, 100)) {
               x <-
-                sample(seq(
-                  from = -00,
-                  to = 100,
-                  by = 1
-                ),
-                size = round(runif(1, min = 10, max = 40)),
-                replace = TRUE)
+                sample(seq(from = 0,
+                           to = 100,
+                           by = 1),
+                       size = round(runif(1, min = 10, max = 40)),
+                       replace = TRUE)
               # Check same length
-              histDiff <- variableStars::diffHistogram(x, 400)
+              histDiff <- diffHistogram(x, 400)
               expect_equal(length(histDiff$histogram$bins),
                            length(histDiff$histogram$values))
             }
@@ -75,9 +93,28 @@ test_that("diffHistogram returns a histogram with equal length between bins and 
 test_that("diffHistogram returns a specific histogram",
           {
             x <- c(1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1)
-            histDiff <- variableStars::diffHistogram(x, 400)
+            histDiff <- diffHistogram(x, 400)
             for (i in seq(1:(length(x)))) {
               expect_equal(histDiff$histogram$values[i],
                            as.numeric(histDiff$histogram$values)[i])
+            }
+          })
+
+context("Signal tools")
+
+test_that("findPeaks returns at least one peak and sorted on random data",
+          {
+            for (i in seq(1, 100)) {
+              x <-
+                sample(seq(
+                  from = -1000,
+                  to = 1000,
+                  by = 1
+                ),
+                size = round(runif(1, min = 10, max = 40)),
+                replace = TRUE)
+              peaks <- variableStars::findPeaks(x)
+              expect_gt(length(peaks), 0)
+              expect_true(all(diff(peaks) > 0))
             }
           })
