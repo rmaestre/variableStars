363a278a2e81c265c6d8c7ad345c818f91481e60
roberto.maestre <roberto.maestre@bbvadata.com>
2018-11-18 20:15:43 +0100

    Tests added

--- tests/testthat/test_tools.R
+++ tests/testthat/test_tools.R
@@ -3,6 +3,22 @@ library(variableStars)
 
 context("Differences tools")
 
+test_that("differences returns an error with an empty vector", {
+  expect_error(differences(as.numeric(c())))
+})
+
+test_that("differences returns numbers but not zeros", {
+  for (i in seq(1, 100)) {
+    x <- sample(seq(from = 0,
+                 to = 100,
+                 by = 1),
+             size = round(runif(1, min = 10, max = 40)),
+             replace = TRUE)
+    # Check same length
+    expect_equal(sum(differences(x) == 0), 0)
+  }
+})
+
 test_that("differences returns an empty vector if all differences are 0", {
   for (i in seq(1, 100)) {
     number <- round(runif(1, min = -1000, max = 1000))
@@ -21,7 +37,9 @@ test_that("differences returns the exact abs difference between two numbers",
               numberA <- round(runif(1, min = -1000, max = 1000))
               numberB <- round(runif(1, min = -1000, max = 1000))
               # Check same length
-              expect_equal(differences(c(numberA, numberB))[1], abs(numberA - numberB))
+              if (numberA != numberB){
+                expect_equal(differences(c(numberA, numberB))[1], abs(numberA - numberB))
+              }
             }
           })
 
@@ -36,7 +54,6 @@ test_that("differences returns the combination of all differences", {
   expect_equal(diffs[2], abs(numberA - numberC))
   expect_equal(diffs[3], abs(numberB - numberC))
 })
-
 
 test_that("differences returns the combination of all differences without zeros",
           {
@@ -56,7 +73,6 @@ test_that("differences returns the combination of all differences without zeros"
             expect_equal(diffs[5], abs(numberC - numberD))
           })
 
-
 test_that("adjacentDifferences returns the same output as diff",
           {
             for (i in seq(1, 100)) {
@@ -68,6 +84,21 @@ test_that("adjacentDifferences returns the same output as diff",
                        replace = TRUE)
               # Check same output
               expect_true(all(adjacentDifferences(x) == diff(x)))
+            }
+          })
+
+test_that("adjacentDifferences returns one element less than the input",
+          {
+            for (i in seq(1, 100)) {
+              x <-
+                sample(seq(from = 0,
+                           to = 100,
+                           by = 1),
+                       size = round(runif(1, min = 10, max = 40)),
+                       replace = TRUE)
+              # Check same output
+              expect_lt(length(adjacentDifferences(x)), length(x))
+              expect_equal(length(adjacentDifferences(x))+1, length(x))
             }
           })
 
@@ -113,7 +144,7 @@ test_that("findPeaks returns at least one peak and sorted on random data",
                 ),
                 size = round(runif(1, min = 10, max = 40)),
                 replace = TRUE)
-              peaks <- variableStars::findPeaks(x)
+              peaks <- findPeaks(x)
               expect_gt(length(peaks), 0)
               expect_true(all(diff(peaks) > 0))
             }
