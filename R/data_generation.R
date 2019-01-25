8e45117a24fb4d22cbae3d9a29342bfb21ca81e1
roberto.maestre <roberto.maestre@bbvadata.com>
2019-01-25 18:37:42 +0100

    Fix bug on Dirac patterns generation

--- R/data_generation.R
+++ R/data_generation.R
@@ -1,31 +1,42 @@
 #' @export
-generate_data <- function(numFreqs, distance, baseAMplitudeFirst, baseAMplitudeSecond, shift, seed,
-                          freqOneRandRange, freqTwoRandRange, ampRandRange) {
-  # Create number of frecuences
-  n <- round(numFreqs / 2)
-  dis <- distance
+generate_data <- function(numFreqs,
+                          distance,
+                          periodF,
+                          periodS,
+                          baseAMplitudeFirst,
+                          baseAMplitudeSecond,
+                          seed,
+                          freqOneRandRange,
+                          freqTwoRandRange,
+                          ampRandRange) {
   # Create data.frame
-  dt <- data.frame("x"=seq(from=0, to=2*n*dis, by=dis), "y"=baseAMplitudeFirst)
-  dt$pos <- seq(1,nrow(dt)) # Sequencial integers
-  # Even elements with other amplitude value
-  dt[lapply(dt$pos, "%%", 2) == 0,]$y <- baseAMplitudeSecond
+  dt <-
+    data.frame(
+      "x" = seq(from = 0, to = numFreqs - 1, by = periodF),
+      "y" = baseAMplitudeFirst,
+      "pattern" = 1
+    )
+  dtDos <-
+    data.frame(
+      "x" = seq(from = distance, to = numFreqs - 1, by = periodS),
+      "y" = baseAMplitudeSecond,
+      "pattern" = 2
+    )
+  dt <- rbind(dt, dtDos)
   
-  # Add shift displacement ob the first pattern
-  dt[lapply(dt$pos, "%%", 2) == 0,]$x <- dt[lapply(dt$pos, "%%", 2) == 0,]$x + shift
-  
-  # Apply random 
+  # Apply random
   set.seed(seed)
   # Add random noise on frequence
   if (freqOneRandRange > 0) {
-    dt[lapply(dt$pos, "%%", 2) == 0,]$x <- rnorm(n, 
-                                                 dt[lapply(dt$pos, "%%", 2) == 0,]$x, 
-                                                 freqOneRandRange)
+    dt[dt$pattern == 1, ]$x <-
+      rnorm(nrow(dt[dt$pattern == 1, ]), dt[dt$pattern == 1, ]$x , freqOneRandRange)
   }
   if (freqTwoRandRange > 0) {
-    dt[lapply(dt$pos, "%%", 2) != 0,]$x <- rnorm(n+1, 
-                                                 dt[lapply(dt$pos, "%%", 2) != 0,]$x, 
-                                                 freqTwoRandRange)
+    dt[dt$pattern == 2, ]$x <-
+      rnorm(nrow(dt[dt$pattern == 2, ]), dt[dt$pattern == 2, ]$x , freqTwoRandRange)
   }
   dt$y <- abs(rnorm(nrow(dt), dt$y, ampRandRange))
+  # Sort by frecuency
+  dt <- dt[order(dt$x),]
   return(dt)
 }
