ef30515840d62f1546eaae389331bd5ffa259a05
roberto.maestre <roberto.maestre@bbvadata.com>
2019-03-05 19:06:47 +0100

    Data genration of oscilation modes added (l=1,m=0,m=+-1)

--- R/data_generation.R
+++ R/data_generation.R
@@ -40,3 +40,103 @@ generate_data <- function(numFreqs,
   dt <- dt[order(dt$x),]
   return(dt)
 }
+
+
+
+#' @export
+generate_data_modes <- function(deltaNu, nuRange, deltaR, numPoints) {
+  # DS to return data
+  df <- data.frame()
+  # Range for data generation
+  range <- c(nuRange[1] * deltaNu, nuRange[2] * deltaNu)
+  
+  # L0 distance from origin
+  range_dl0 <- c(0, 10)
+  
+  # Avoid negative ranges
+  dl0 <- round(runif(1, range_dl0[1], range_dl0[2]), 2)
+  while (range[2] - (range[1] + dl0)<0) {
+    dl0 <- round(runif(1, range_dl0[1], range_dl0[2]), 2)
+  }
+  # Vector generation
+  l0 <- seq(
+    from = range[1] + dl0,
+    to = range[2],
+    by = (range[2] - (range[1] + dl0)) / numPoints
+  )[1:numPoints]
+  # Append data
+  df <-
+    rbind(df, data.frame(
+      "frequency" = l0,
+      "mode" = "l0",
+      "amplitude" = 1
+    ))
+  
+  
+  # L1 distance from origin
+  dl1 <-
+    round(runif(1, 0, l0[2] - l0[1]), 2) # Rand betwee first and second L2
+  # Vector generation
+  print("-----")
+  print(deltaNu)
+  print(dl0)
+  print(range[1] + dl0)
+  print(range[2])
+  print((range[2] - (range[1] + dl0)) / numPoints)
+  
+  print(range)
+  print(l0)
+  print(dl1)
+  print(l0[1] + dl1)
+  print("-----")
+  l1 <- seq(
+    from = l0[1] + dl1,
+    to = range[2],
+    by = (range[2] - (l0[1] + dl1)) / numPoints
+  )[1:numPoints]
+  
+  # Append data
+  df <-
+    rbind(df,
+          data.frame(
+            "frequency" = l1,
+            "mode" = "l1",
+            "amplitude" = 0.8
+          ))
+  
+  
+  # M-1, M1 distance from l1
+  dr <- round(runif(1, 0, deltaNu), 2)
+  
+  mp1 <- l1 + dr
+  ml1 <- l1 - dr
+  
+  # Append data
+  df <-
+    rbind(df,
+          data.frame(
+            "frequency" = mp1,
+            "mode" = "mp1",
+            "amplitude" = 0.75
+          ))
+  df <-
+    rbind(df,
+          data.frame(
+            "frequency" = ml1,
+            "mode" = "ml1",
+            "amplitude" = 0.75
+          ))
+  
+  stopifnot(length(l0) == numPoints) # Length assert for L0
+  stopifnot(length(l1) == numPoints) # Length assert for L0
+  stopifnot(length(mp1) == numPoints) # Length assert for L0
+  stopifnot(length(ml1) == numPoints) # Length assert for L0
+  
+  return(list(
+    "data" = df,
+    "dl0" = dl0,
+    "dl1" = dl1,
+    "dr" = dr,
+    "dnu" = deltaNu
+  ))
+}
